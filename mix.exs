9ae2ebb0d93ec3e300e95bf0639922d5adfb60a9
Wojciech Kaczmarek <frk@kofeina.net>
2014-01-25 16:54:26 +0100

    historical moment where plug'n'cowboy deps arrive

--- mix.exs
+++ mix.exs
@@ -5,12 +5,12 @@ defmodule Rockside.Mixfile do
     [ app: :rockside,
       version: "0.0.1",
       elixir: "~> 0.12.2",
-      deps: deps ]
+      deps: deps Mix.env ]
   end
 
   # Configuration for the OTP application
   def application do
-    []
+    [ applications: [:cowboy, :plug] ]
   end
 
   # Returns the list of dependencies in the format:
@@ -18,7 +18,11 @@ defmodule Rockside.Mixfile do
   #
   # To specify particular versions, regardless of the tag, do:
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
-  defp deps do
-    []
+  defp deps(:prod) do
+    [ {:cowboy, github: "extend/cowboy"},
+      {:plug, "~> 0.2.0", github: "elixir-lang/plug"},
+    ]
   end
+
+  defp deps(_), do: deps :prod
 end
