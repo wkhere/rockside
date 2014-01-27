558a49f5b85c0040ef220f5eeee5fc88d5c09d98
Wojciech Kaczmarek <frk@kofeina.net>
2014-01-27 22:59:55 +0100

    plugged placeholder responding to http requests

--- lib/rockside.ex
+++ lib/rockside.ex
@@ -1,2 +1,17 @@
 defmodule Rockside do
+  def run_sanity() do
+    Plug.Adapters.Cowboy.http Rockside.Sanity, []
+  end
+end
+
+defmodule Rockside.Sanity do
+  import Plug.Connection
+
+  def call(conn, []) do
+    #:timer.sleep 1000
+    conn = conn
+      |> put_resp_content_type("text/plain")
+      |> send_resp(200, "Pong")
+    {:ok, conn}
+  end
 end
