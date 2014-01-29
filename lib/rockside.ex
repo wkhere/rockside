b56c72ce073693729aa8b1d53634bf44f81a487b
Wojciech Kaczmarek <frk@kofeina.net>
2014-01-29 17:16:42 +0100

    very basic html builder - without attrs yet

--- lib/rockside.ex
+++ lib/rockside.ex
@@ -4,14 +4,30 @@ defmodule Rockside do
   end
 end
 
+defmodule Rockside.Doc do
+
+  def tag(tag, inner//[]) do
+    ["<#{tag}>", inner, "</#{tag}>"]
+  end
+
+  def finish(chunks), do: chunks |> List.flatten |> Enum.join
+
+  def html(inner), do: tag(:html, inner)
+end
+
 defmodule Rockside.Sanity do
   import Plug.Connection
+  import Rockside.Doc
 
   def call(conn, []) do
-    #:timer.sleep 1000
+    doc = html [
+      tag(:head),
+      tag(:body, "foo")
+    ]
     conn = conn
-      |> put_resp_content_type("text/plain")
-      |> send_resp(200, "Pong")
+      |> put_resp_content_type("text/html")
+      |> send_resp(200, finish doc)
     {:ok, conn}
   end
 end
+
