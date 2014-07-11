2059ec1d9008e06ae009fac2fe9e2922177699e0
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-11 14:25:52 +0200

    grid() bench using benchfella

---
+++ bench/tag_bench.exs
@@ -0,0 +1,19 @@
+defmodule BenchHelper do
+  def rand(x) when is_integer(x), do: :random.uniform(x)
+  def rand(coll) when is_list(coll), do: coll |> Enum.shuffle |> hd
+end
+
+defmodule Rockside.TagBench do
+  use Benchfella
+  import Rockside.Doc
+  import BenchHelper
+
+  @grid_attrs \
+    [{:wide, rand(16)},  {rand([:preffix,:suffix]), rand(16)}]
+      |> Enum.into(rand([[], [:alpha], [:omega]]))
+      |> Enum.into([ rand([] ++ (for x <- [:class,:id,:style], do: {x, :val})) ])
+
+  bench "grid element creation" do
+    grid(@grid_attrs, [])
+  end
+end
