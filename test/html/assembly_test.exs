4ee69025e987eebade332a12c98341a1f02aefe1
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-22 20:10:09 +0200

    totally basic html assembly

---
+++ test/html/assembly_test.exs
@@ -0,0 +1,93 @@
+alias Rockside.HTML
+alias HTML.Assembly
+
+defmodule Assembly.Test do
+  use    ExUnit.Case
+  import HTML.TestHelper
+  import HTML.TagBase
+  import Assembly.St
+
+  test "new state has empty stack" do
+    assert new.stack == []
+  end
+
+  test "push anything" do
+    assert ( new |> push(1) ).stack == [1]
+  end
+
+  test "2x push anything" do
+    assert ( new |> push(1) |> push(2) ).stack == [2,1]
+  end
+
+  test "2x push anything + release" do
+    assert new |> push(1) |> push(2) |> release == [1,2]
+  end
+
+  test "push one tag" do
+    assert new |> push(tag(:foo, "bar")) |> release |> flush
+      == "<foo>bar</foo>"
+  end
+
+  test "push two tags" do
+    assert new
+      |> push(tag(:foo, "1"))
+      |> push(tag(:bar, "2"))
+      |> release
+      |> flush
+      == "<foo>1</foo><bar>2</bar>"
+  end
+
+  test "push two tags nested and a third sister to first" do
+    assert new
+      |> push(tag(:foo,
+          new
+          |> push(tag(:bar, "inner"))
+          |> release
+        ))
+      |> push(tag(:quux, "sister"))
+      |> release
+      |> flush
+      == """
+      <foo><bar>inner</bar></foo>
+      <quux>sister</quux>
+      """ |> no_lf
+  end
+
+  test "mix nesting and a list content" do
+    assert new
+      |> push(tag(:foo, [
+          "inner",
+          new
+          |> push(tag(:bar, "child"))
+          |> release
+        ]))
+      |> push(tag(:quux, "sister"))
+      |> release
+      |> flush
+      === """
+      <foo>inner<bar>child</bar></foo>
+      <quux>sister</quux>
+      """ |> no_lf
+  end
+
+  test "push two sister tags and then two nested in 2nd sis" do
+    assert new
+      |> push(tag(:foo, "sister"))
+      |> push(tag(:bar,
+          new
+          |> push(tag(:quux, "inner"))
+          |> push(tag(:cosmic, "inner as well"))
+          |> release
+        ))
+      |> release
+      |> flush
+      == """
+      <foo>sister</foo>
+      <bar>
+      <quux>inner</quux>
+      <cosmic>inner as well</cosmic>
+      </bar>
+      """ |> no_lf
+  end
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,9 @@ ExUnit.start
 
 defmodule Rockside.HTML.TestHelper do
   def flush(doc) do
-    doc |> Rockside.HTML.TagBase.Tools.flush |> String.replace("\n", "")
+    doc |> Rockside.HTML.TagBase.Tools.flush |> no_lf
   end
+
+  def no_lf(s), do: String.replace(s, "\n", "")
+  def no_ws(s), do: Regex.replace(~r/\s/, s, "")
 end
