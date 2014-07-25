e404c850d4cafa8aedeb92057710a242f5cbc14f
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-25 16:45:59 +0200

    refac assembly tools/types, were stuck in tagbase
    This is preparation for having attributes in the DSL :]

--- lib/html/tagbase.ex
+++ lib/html/tagbase.ex
@@ -1,13 +1,12 @@
-defmodule Rockside.HTML.TagBase do
+alias Rockside.HTML
 
-  @type tagname  :: atom
-  @type attrs    :: [{atom, String.t}]
-  @type content  :: String.t | out_tag | [String.t | out_tag]
-  @type out_tag1 :: [String.t | [String.t]]
-  @type out_tag  :: [String.t | [String.t] | content]
+defmodule HTML.TagBase do
 
-  @spec tag(tagname, attrs, content)  :: out_tag
-  @spec tag(tagname)                  :: out_tag
+  alias HTML.Assembly.Types, as: T
+  import HTML.Assembly.Tools, only: [htmlize_attrs: 1]
+
+  @spec tag(T.tagname, T.attrs, T.content)  :: T.out_tag
+  @spec tag(T.tagname)                      :: T.out_tag
 
   def tag(tag, attrs\\[], inner)
 
@@ -21,7 +20,7 @@ defmodule Rockside.HTML.TagBase do
   def tag(tag), do: tag(tag, [], [])
 
 
-  @spec tag1(tagname, attrs) :: out_tag1
+  @spec tag1(T.tagname, T.attrs) :: T.out_tag1
 
   def tag1(tag, attrs\\[])
 
@@ -31,28 +30,4 @@ defmodule Rockside.HTML.TagBase do
   def tag1(tag, attrs) do
       ["\n<#{tag} ", htmlize_attrs(attrs), " />"]
   end
-
-
- @spec htmlize_attrs(attrs) :: [String.t]
-
-  defp htmlize_attrs(attrs) do
-    Enum.map(attrs, fn {k,v} ->
-      k = k |> to_string |> String.replace("_", "-")
-      ~s/#{k}="#{v}"/
-    end)
-      |> Enum.intersperse(" ")
-  end
-
-  defmodule Tools do
-    alias TagBase, as: T
-
-    @spec flush(T.out_tag) :: String.t
-
-    def flush(chunks) when is_list(chunks) do
-      chunks |> List.flatten |> Enum.join
-    end
-    # flush/1 needed only for plug-free tests, because patched Plug
-    # accepts iolist as a resp body
-  end
-
- end
+end
