6cc7eefacc0e9f3b98f87799ccacccc5d74df86c
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-14 20:47:33 +0200

    sanity module now uses dsl :)

--- lib/sanity.ex
+++ lib/sanity.ex
@@ -14,10 +14,24 @@ defmodule Rockside.Sanity do
     end
   end
 
+  defmodule Doc.ViaDSL do
+    def doc do
+      import Rockside.HTML.DSL
+      builder do html do
+        head do
+          meta http_equiv: "Content-Type", content: "text/html"
+          title "foo"
+        end
+        body "foo"
+      end end
+    end
+  end
+
   def init(opts), do: opts
 
   def call(conn, []) do
-    import Doc.ViaElements
+    #import Doc.ViaElements
+    import Doc.ViaDSL
     conn = conn
       |> put_resp_content_type("text/html")
       |> send_resp(200, doc)
