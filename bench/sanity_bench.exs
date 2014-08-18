ba10a0b340d2d24af23fecca089e6121e5dfcb54
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-18 15:50:14 +0200

    bench: rm ref to code removed in 82d07bd0a
    benchfella still not working anyway

--- bench/sanity_bench.exs
+++ bench/sanity_bench.exs
@@ -2,14 +2,7 @@ defmodule Rockside.Sanity.Doc.Bench do
   use Benchfella
   alias Rockside.Sanity.Doc
 
-  bench "doc via elements api" do
-    Doc.ViaElements.doc
-  end
-
   bench "doc via dsl" do
     Doc.ViaDSL.doc
   end
-
-  # idea is to but here other Sanity.Doc representations
-  # coded in different layers / DSLs
 end
