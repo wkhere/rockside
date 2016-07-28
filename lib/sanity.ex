c22968c8a8afdaea5db5a1f9b2fa3584b988c8af
Wojciech Kaczmarek <wojtekk@kofeina.net>
2016-07-28 03:57:00 +0200

    send only text response - for benchmark

--- lib/sanity.ex
+++ lib/sanity.ex
@@ -1,26 +1,26 @@
 defmodule Rockside.Sanity do
   import Plug.Conn
 
-  defmodule Doc.ViaDSL do
-    def doc do
-      use WebAssembly
-      builder do html do
-        head do
-          meta http_equiv: "Content-Type", content: "text/html"
-          title "foo"
-        end
-        body "foo"
-      end end
-    end
-  end
+#  defmodule Doc.ViaDSL do
+#    def doc do
+#      use WebAssembly
+#      builder do html do
+#        head do
+#          meta http_equiv: "Content-Type", content: "text/html"
+#          title "foo"
+#        end
+#        body "foo"
+#      end end
+#    end
+#  end
 
   def init(opts), do: opts
 
   def call(conn, []) do
-    import Doc.ViaDSL
+    #import Doc.ViaDSL
     conn = conn
-      |> put_resp_content_type("text/html")
-      |> send_resp(200, doc)
+      |> put_resp_content_type("text/plain")
+      |> send_resp(200, "Hello World")
     conn
   end
 end
